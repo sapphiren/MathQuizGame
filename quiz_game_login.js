4adc92c21758e374e7dda5dfa2c1d1efff905511
sapphiren <117869067+sapphiren@users.noreply.github.com>
2023-06-13 20:14:54 -0400

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,7 @@
+function addUser(){
+    player1_name = document.getElementById("player1name").value;
+    player2_name = document.getElementById("player2name").value;
+    
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+}
